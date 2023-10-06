0f1e8a7f8ed91c2fd25d895ab8f1cd3418c4b0c3
Lazarev Konstantin <Lazarev-konstant@mail.ru>
2023-10-06 21:01:36 +0300

    Add AText

---
+++ components/ui/atoms/text/index.vue
@@ -0,0 +1,107 @@
+<template>
+	<div>
+		<span v-if="tag === 'span'" :class="currentClasses">
+			<slot />
+		</span>
+		<p v-else-if="tag === 'p'" :class="currentClasses">
+			<slot />
+		</p>
+		<div v-else-if="tag === 'div'" :class="currentClasses">
+			<slot />
+		</div>
+		<h1 v-else-if="tag === 'h1'" :class="currentClasses">
+			<slot />
+		</h1>
+		<h2 v-else-if="tag === 'h2'" :class="currentClasses">
+			<slot />
+		</h2>
+		<h3 v-else-if="tag === 'h3'" :class="currentClasses">
+			<slot />
+		</h3>
+		<h4 v-else-if="tag === 'h4'" :class="currentClasses">
+			<slot />
+		</h4>
+		<h5 v-else-if="tag === 'h5'" :class="currentClasses">
+			<slot />
+		</h5>
+		<h6 v-else-if="tag === 'h6'" :class="currentClasses">
+			<slot />
+		</h6>
+	</div>
+</template>
+<script>
+export default {
+	name: 'AText',
+	data: () => {
+		return {
+			currentClasses: []
+		}
+	},
+	props: {
+		attributes: {
+			default: {},
+			type: Object
+		},
+		tag: {
+			type: String,
+			default: 'span'
+		}
+	},
+	mounted() {
+		const classes = []
+		const availableWeight = [
+			'thin',
+			'extra-light',
+			'light',
+			'regular',
+			'medium',
+			'semi-bold',
+			'bold',
+			'extra-bold',
+			'black'
+		]
+		const availableSize = ['small', 'medium', 'large', 'x-large', '2x-large']
+		const availableDecoration = [
+			'underline',
+			'overline',
+			'line-through',
+			'blink',
+			'solid',
+			'wavy',
+			'dotted',
+			'dashed',
+			'double'
+		]
+		const availableColor = [
+			'cairo',
+			'calgary',
+			'cordoba',
+			'callao',
+			'cucuta',
+			'colombo',
+			'cancun',
+			'cochin',
+			'cardiff',
+			'cleveland'
+		]
+		const availableAlign = ['left', 'right', 'center', 'justify']
+		if ('weight' in this.attributes && availableWeight.includes(this.attributes.weight)) {
+			classes.push(`text_weight_${this.attributes.weight}`)
+		}
+		if ('size' in this.attributes && availableSize.includes(this.attributes.size)) {
+			classes.push(`text_size_${this.attributes.size}`)
+		}
+		if ('decoration' in this.attributes && availableDecoration.includes(this.attributes.decoration)) {
+			classes.push(`text_decoration_${this.attributes.decoration}`)
+		}
+		if ('color' in this.attributes && availableColor.includes(this.attributes.color)) {
+			classes.push(`text_color_${this.attributes.color}`)
+		}
+		if ('align' in this.attributes && availableAlign.includes(this.attributes.align)) {
+			classes.push(`text_align_${this.attributes.align}`)
+		}
+		this.currentClasses = classes.join(' ')
+	}
+}
+</script>
+<style scoped></style>
--- pages/dev/index.vue
+++ pages/dev/index.vue
@@ -1,140 +1,40 @@
 <template>
 	<div>
 		<header>Header!</header>
+		<main class="main_page">
+			<div class="container">
+				<SliderContainer />
+				<AText tag="h1" :attributes="mainTitleSettings.DC">Найбільша база даних про онлайн-казино у світі</AText>
+			</div>
+		</main>
 		<footer>Footer!</footer>
-		<div class="sliderContainer">
-			<Slider :settings="sliderSettings">
-				<div class="card">
-					<div class="item">
-						1
-					</div>
-				</div>
-				<div class="card">
-					<div class="item">
-						2
-					</div>
-				</div>
-				<div class="card">
-					<div class="item">
-						3
-					</div>
-				</div>
-				<div class="card">
-					<div class="item">
-						4
-					</div>
-				</div>
-			</Slider>
-		</div>
-		<div class="sliderContainer">
-			<Slider :settings="sliderSettingsSecond">
-				<div class="cardSecond">
-					<div class="item">
-						1
-					</div>
-				</div>
-				<div class="cardSecond">
-					<div class="item">
-						2
-					</div>
-				</div>
-				<div class="cardSecond">
-					<div class="item">
-						3
-					</div>
-				</div>
-				<div class="cardSecond">
-					<div class="item">
-						4
-					</div>
-				</div>
-				<div class="cardSecond">
-					<div class="item">
-						5
-					</div>
-				</div>
-				<div class="cardSecond">
-					<div class="item">
-						6
-					</div>
-				</div>
-				<div class="cardSecond">
-					<div class="item">
-						7
-					</div>
-				</div>
-				<div class="cardSecond">
-					<div class="item">
-						8
-					</div>
-				</div>
-				<div class="cardSecond">
-					<div class="item">
-						9
-					</div>
-				</div>
-				<div class="cardSecond">
-					<div class="item">
-						10
-					</div>
-				</div>
-			</Slider>
-		</div>
-		<SliderContainer />
 	</div>
 </template>
 
 <script>
 import Slider from '~/components/slider'
 import SliderContainer from '~/components/slider_container'
+import AText from '~/components/ui/atoms/text'
 export default {
 	name: 'dev-main-page',
-	components: { Slider, SliderContainer },
+	components: { Slider, SliderContainer, AText },
 	layout: 'dev',
 	data: () => {
 		return {
-			sliderSettings: {
-				slidesToShow: 1,
-				centerMode: true,
-				autoplay: true,
-				speed: 2000,
-				autoplaySpeed: 2000,
-				initialSlide: 0,
-				infinite: true
-			},
-			sliderSettingsSecond: {
-				slidesToShow: 5,
-				centerMode: false,
-				autoplay: true,
-				speed: 2000,
-				autoplaySpeed: 2000,
-				initialSlide: 0,
-				infinite: true
+			mainTitleSettings: {
+				DC: {
+					align: 'center',
+					weight: 'extra-bold',
+					color: 'calgary',
+					size: '2x-large',
+					decoration: 'underline',
+					class: 'custom_class'
+				},
+				TABLET: {},
+				MOBILE: {}
 			}
 		}
 	}
 }
 </script>
-<style scoped>
-.card {
-	height: 150px;
-	padding: 15px;
-}
-.item {
-	background: red;
-	height: 100%;
-	display: flex;
-	align-items: center;
-	justify-content: center;
-}
-.sliderContainer {
-	width: 400px;
-	box-sizing: border-box;
-	padding: 0px;
-	margin: 0 auto;
-}
-.cardSecond {
-	height: 50px;
-	padding: 15px;
-}
-</style>
+<style scoped></style>
