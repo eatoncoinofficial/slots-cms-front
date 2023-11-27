8e4468b6426d43af80d5b61b19452b018ea68e79
Lazarev Konstantin <Lazarev-konstant@mail.ru>
2023-11-27 21:04:57 +0200

    fix post type

--- pages/casino/_id.vue
+++ pages/casino/_id.vue
@@ -139,7 +139,7 @@ export default {
 		if (route.params.id) {
 			const request = new DAL_Builder()
 			const response = await request
-				.postType('casino_good')
+				.postType('casino')
 				.url(route.params.id)
 				.get()
 			if (response.data.confirm === 'error') {
